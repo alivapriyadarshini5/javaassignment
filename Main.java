eee75a468d6b12ebdfcef7746d97160f0e463541
alivapriyadarshini5 <69763236+alivapriyadarshini5@users.noreply.github.com>
2020-10-11 20:13:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.employee;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		  Employee obj1=new Employee();
+		  Employee obj2=new Employee();
+	        obj1.display1();
+	        obj2.display2();
+
+	}
+
+}
