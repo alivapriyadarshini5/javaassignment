5bfb077ca94aaded2ff63420df2c44fb14fe6ce0
alivapriyadarshini5 <69763236+alivapriyadarshini5@users.noreply.github.com>
2020-10-11 20:14:37 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.employee;
+
+	
+
+	public class Employee{
+		String name1 = "Aliva";
+		int age1=21;
+		String city1= "Bhubaneswar";
+		String name2 = "Akash";
+		int age2=17;
+		String city2= "Kendrapara";
+        String str1 = "My name is " ;
+        String str2 = "My age is " ;
+        String str3 = "My city is " ;
+
+		public void display1() {
+			System.out.println(str1+name1);
+			System.out.println(str2+age1);
+			System.out.println(str3+city1);
+		}
+		public void display2() {
+			System.out.println(str1+name2);
+			System.out.println(str2+age2);
+			System.out.println(str3+city2);
+		}
+
+
+	}
+
+
